dc2d97712f79ab8d7e702b373f06984fddb60343
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-04-29 16:10:41 -0300

    Implementing analyze in sonarCloud

--- src/test/java/com/fiap/challenge/payment/PaymentApiApplicationTests.java
+++ src/test/java/com/fiap/challenge/payment/PaymentApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.fiap.challenge.payment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class PaymentApiApplicationTests {
 
 	@Test
